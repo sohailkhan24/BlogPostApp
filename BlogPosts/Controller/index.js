741b97b1d59379df4106a7f3cccfcc34e8351300
Sohail Khan <48204572+sohailkhan24@users.noreply.github.com>
2021-03-26 16:41:55 +0530

    adding blog to static list
    Blog added(without db)

---
+++ BlogPosts/Controller/index.js
@@ -0,0 +1,64 @@
+//to require and declare things
+const express = require("express");
+const path =require("path");
+const port = 8000;
+const app = express();
+
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'../views'));
+app.use(express.urlencoded({extended:true}));
+
+//blogs
+var blogs=[
+    {
+        Blogtitle:"he",
+        Blogcategory:"heman",
+        Blogcontent:"I am heman HAHAHAHH"
+    }
+];
+
+
+//Controllers
+
+//index or home controller
+app.get('/',function(req,res){
+    
+    return res.render('BlogIndex',{ 
+        title:"Blogs",
+        blogs:blogs,
+    });
+});
+
+
+
+//blogDetails controller
+app.get('/BlogDetails',function(req,res){
+    return res.render('BlogDetails',{ title:"Blog Description"});
+});
+
+
+//new blog controller
+app.get('/NewBlog',function(req,res){
+    return res.render('NewBlog',{ title:"New Blog"});
+});
+
+app.post('/newblog',function(req,res){
+    blogs.push({
+        Blogtitle:req.body.Blogtitle,
+        Blogcategory:req.body.Blogcategory,
+        Blogcontent:req.body.Blogcontent
+    })
+    return res.redirect('/');
+});
+
+
+
+//Server start and error
+app.listen(port,function(err){
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log("Server is up and runing on PORT: 127.0.0.1:",port);
+});
